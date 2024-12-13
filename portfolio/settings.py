b097dedd6404de73e287979f9a526460a1e1c3f0
Praneeth G <praneeth2630@gmail.com>
2024-12-13 21:21:48 +0530

    images

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'main/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'main/staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/staticfiles')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'main/static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/staticfiles/images')
